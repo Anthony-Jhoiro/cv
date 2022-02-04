fcbbefe818b3dc153bb9b305d2e09e8c381db580
anthony <quereantho@gmail.com>
2022-02-04 16:51:06 +0100

    Add a print button

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 (() => {
 
-    const cv = document.getElementById("cv-container");
+    const resumeContainer = document.getElementById("cv-container");
+    const printButton = document.getElementById("print-btn")
 
-    function printDiv({ divId, title }) {
+    function printResume() {
         let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
 
-        mywindow.document.write(`<html><head><title>${title}</title>`);
+        mywindow.document.write(`<html><head><title>Anthony Quéré</title>`);
+        // Link fonts
         mywindow.document.write('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap" rel="stylesheet">');
+        // Link stylesheet
         mywindow.document.write('<link rel="stylesheet" type="text/css" href="/style.css" />')
+
         mywindow.document.write('</head><body >');
-        mywindow.document.write(cv.innerHTML);
+        mywindow.document.write(resumeContainer.innerHTML);
         mywindow.document.write('</body></html>')
 
         mywindow.document.close(); // necessary for IE >= 10
@@ -17,15 +21,10 @@
 
         mywindow.print();
 
-        return true;
+        // Use a timeout before closing the window to prevent the window from closing before the 
+        // PDF export (needed to work on Brave browser)
+        setTimeout(() => mywindow.close(), 1000);
     }
 
-    cv.addEventListener("click", () => {
-        printDiv({ divId: "cv-container", ttle: "Anthony Quéré" })
-    });
-
-
-
-
-
+    printButton.onclick = printResume;
 })();
